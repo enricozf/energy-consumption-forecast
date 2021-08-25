51626243ce2b8a0c1bd65afcc7cfb6211508dc45
Enrico <enrico.ferreira@industriall.ai>
2021-08-24 23:12:30 -0300

    FEAT: Junta os dados de todos SM em um só DataFrame.

--- Code/main.py
+++ Code/main.py
@@ -1,29 +1,34 @@
 #%%
 import pandas as pd
+from os.path import normpath, join
 from utils.exploratory_data_analysis import (read_acorn_group_blocks, 
                                              split_into_acorns)
 
 def create_df(
     consumption_file_path : str ='..//Data//halfhourly_dataset',
     sm_info_file : str = '..//Data//informations_households.csv',
-    lst_acorn_names : str = ['Affluent', 'Comfortable', 'Adversity'],
-    blocks_per_acorn : dict = {
-        'Affluent' : (0, 43),
-        'Comfortable' : (43, 73),
-        'Adversity' : (74, 110)},
-    day_col : str = 'tstp',
-    save_filename : str =None,
-    save_path : str = '..//Figures//',
-    fontsize=10):
+    desired_acorn_name : str = 'Affluent'):
     
-    # Read all Smart meters data
-    dict_data = read_acorn_group_blocks(consumption_file_path)
-    # Load smart meters information
-    df_sm_info = pd.read_csv(sm_info_file, header=0)
-    # Split into acorns
-    dict_data = split_into_acorns(dict_data, df_sm_info)
+    # Get all SM id from desired acorn and remove ToU SMs
+    sm_info_df = pd.read_csv(sm_info_file, header=0)
+    sm_info_df = sm_info_df[sm_info_df['stdorToU']=='Std']
+    sm_info_df = sm_info_df[sm_info_df['Acorn_grouped']==desired_acorn_name]
+    desired_blocks = list(set(sm_info_df['file'].values))
+    
+    # Read hourly data from desired acorn
+    data_df = pd.DataFrame()
+    for block_file in desired_blocks:
+        print(f'Loading {block_file} file...')
+        file_path = normpath(join(consumption_file_path, block_file + '.csv'))
+        aux_df = pd.read_csv(file_path, header=0).set_index('LCLid')
+        # print('--- Shape: {}'.format(aux_df.shape))
+        aux_df = aux_df.loc[sm_info_df[sm_info_df['file']==block_file]\
+                            ['LCLid'].values]
+        # print('--- Shape: {}'.format(aux_df.shape))
+        data_df = pd.concat([data_df, aux_df.reset_index()], axis=0)
+    print('All datablock files were read.')
 
-    return dict_data
+    return data_df
 
 def monday_or_weekend(elem):
     return 1 if elem.weekday() in [0,5,6] else 0
@@ -36,7 +41,9 @@ if __name__ == '__main__':
     desired_weather_cols = ['apparentTemperature', 'weekly_temperature'] #uvIndex
 
     # TODO: Merge + fill NaN
-    df = pd.read_csv(block_file_name, header=0)
+    df = create_df()
+    df.head()
+    print('Shape: ', df.shape)
     df[time_col_data] = pd.to_datetime(df[time_col_data])
     df['MondayORWE'] = df[time_col_data].apply(monday_or_weekend)
     # df['weekday'] = df[time_col_data].apply(lambda x: x.weekday())
@@ -59,4 +66,6 @@ if __name__ == '__main__':
                       pd.DataFrame(weather_sr).rename(columns={0:w_col}),
                       how='inner', left_index=True, right_index=True)
 
-
+    # TODO: Splitar os dados em input e target
+    # TODO: Splitar os dados em treino teste
+    # TODO: Alimentar e treinar modelo
